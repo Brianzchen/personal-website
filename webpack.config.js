d23bd191b8101a8b00ee740acc2d5c1a07ca95d4
Brian Chen <bchen.chen93@gmail.com>
2017-12-25 13:19:39 +1300

    moved dist into public

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
-  output: { path: path.resolve(__dirname, 'dist/'), filename: 'index.js' },
+  output: { path: path.resolve(__dirname, 'public/dist/'), filename: 'index.js' },
   resolve: {
     extensions: ['*', '.js', '.jsx', '.css'],
     modules: [
