2b36a39fc3708b8b37cb84bbb151bdb336ce1899
Brian Chen <bchen.chen93@gmail.com>
2017-12-28 15:22:41 +1300

    updated experiences

--- src/App/Experience/texts/kiwiplan.js
+++ src/App/Experience/texts/kiwiplan.js
@@ -1,5 +1,8 @@
 export default `
   I started at Kiwiplan in July 2016. Since then I have taken my knowledge of web development to improve the company as best I can.
 
-  In December of 2016, I explored the possibility of updating the companies current suite of web products to ReactJS.
+  In December of 2016, I explored the possibility of updating the companies current suite of web products to ReactJS. Through this, the companies newer products now run on a modern set of front-end processes.
+  The products use ES2015+ with ReactJS + Redux and is compiled through babel with Webpack.
+
+  I also implemented an on site npm server where I host the an inhouse React based gui framework that I created and currently maintain.
 `;
