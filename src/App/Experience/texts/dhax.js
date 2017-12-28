b4126ec2db7debc3f6771bdade8ff6ffc2a43971
Brian Chen <bchen.chen93@gmail.com>
2017-12-29 05:18:54 +1300

    updated experience text

--- src/App/Experience/texts/dhax.js
+++ src/App/Experience/texts/dhax.js
@@ -1,8 +1,8 @@
 export default `
-  I'm a huge advocate of personal projects. I believe it's the best way to learn, the best way to improve. If you want to get better, why not do so while creating something you'll use, that way you'll push yourself.
-  If you find something too difficult to implement you'll force yourself to make it happen because you are the consumer and you know how killer that feature is, and when it's done you will have learnt something new.
+  I'm a huge advocate of personal projects; I believe it's the best way to learn, the best way to improve. If you want to get better, why not do so by creating something you'll use, that way you'll be more motivated to complete it.
+  If you find something that's difficult to implement, you'll force yourself to make it happen because you are the consumer and you know how killer that feature is, and when it's done you will have learnt something new.
 
-  Dhax is my attempt at a personal project at scale. It's not intended to be groundbreaking or super innovative. But to be something that I can work on, to improve personally, to know that I built it's all myself. While also being something that I would personally use on a daily basis instead being a simple app 'hello world' app I build for the sake of building it.
+  Dhax is my attempt to create a personal project at scale. It's not intended to be groundbreaking or super innovative, but to be something that I can work on, to improve personally, to know that I built it all myself. At the same time, to serve as something that I would personally use on a daily basis instead being a simple "hello world" app I built for the sake of building it.
 
-  This project is all about helping me decide where to eat. Have you ever had that feeling where you can't quite decide where to eat? You know all the restaurants in your area that you like but you simply can't pick one or you think you may have forgotten one. Why not let an app decide for you?
+  Have you ever had that feeling where you can't quite decide where to eat? You know all the restaurants in your area that you like but you simply can't pick one or you think you may have forgotten one. Why not let an app decide for you? That's what Dhax does for you.
 `;
--- src/App/Experience/texts/kiwiplan.js
+++ src/App/Experience/texts/kiwiplan.js
@@ -1,8 +1,8 @@
 export default `
-  Kiwiplan is a company I started working for in July 2016. Since then I have taken my knowledge of web development to improve the company as best I can.
+  Kiwiplan is a company I started working at in July 2016. Since then I have taken my knowledge of web development to improve the company the best I can.
 
-  In December of 2016, I explored the possibility of updating the companies current suite of web products to ReactJS. Through this, the companies newer products now run on a modern set of front-end processes.
-  The products use ES2015+ with ReactJS + Redux and is compiled through babel with Webpack. Navigation while previously handled by HTML location changes were updated to use client side routing with react-router to allow for smooth transitions and state retention to allow for smaller network payloads on page changes.
+  In December of 2016, I explored the possibility of updating the companies current suite of web products to ReactJS. Through this, the company's newer products now run on a modern set of front-end processes.
+  The products use ES2015+ with ReactJS + Redux and is compiled through Babel with Webpack. Navigation, while previously handled by HTML location changes, were updated to use client-side routing with react-router to allow smooth transitions and state retention which facilitates smaller network payloads on page changes.
 
-  I also implemented an on site npm server where I host the an inhouse React based gui framework that I created and currently maintain.
+  I also implemented an on-site npm server where I host an inhouse React-based GUI framework that I created and maintain.
 `;
