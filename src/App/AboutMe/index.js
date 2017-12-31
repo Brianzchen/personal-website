0bfdd28772317128f14fff526118207ce01c69dc
Brian Chen <bchen.chen93@gmail.com>
2017-12-31 17:07:34 +1300

    added to summary

--- src/App/AboutMe/index.js
+++ src/App/AboutMe/index.js
@@ -6,28 +6,13 @@ import Text from 'components/Text';
 import { aboutMe } from 'lib/locations';
 
 import Details from './Details';
-
-const text = `
-  Self taught Front-end JavaScript developer focused on ReactJS + Redux.
-
-  Advodate for agile and scrum.
-
-  love to code in my free time and learn about new ways of doing old things more efficiently
-
-  always thinking about how i can do things better and improve the current process.
-
-  Like the follow all the latest trends but want to know why A is chosen instead of B
-
-  My interests outside of coding inclde lifting heavy weights, window shopping, eating lots of food and of course, Sharon Wang.
-
-  My greatest weakness in my opinion is naming variables.
-`;
+import summary from './summary';
 
 const AboutMe = () => (
   <Section location={aboutMe}>
     <Details />
     <Text>
-      {text}
+      {summary}
     </Text>
   </Section>
 );
---
+++ src/App/AboutMe/summary.js
@@ -0,0 +1,28 @@
+export default `
+  Self taught and self motivated JavaScript dev, passionate about the front-end world. Always striving for relentless improvement and a stronger believer that if you want to improve you need a personal project.
+
+  Experienced ReactJS developer with strong CSS3 skills. Confident in recreating any design, from image to interactive page.
+
+
+  I like to develop at home, learn things and take that knowledge back to work and improve my work project and vice versa.
+
+
+  Self taught Front-end JavaScript developer focused on ReactJS + Redux.
+
+  Advodate for agile and scrum. Not interested in a scrum master role but very passionate about the agile methodology and implementing it best i can.
+
+  love to code in my free time and learn about new ways of doing old things more efficiently
+
+  always thinking about how i can do things better and improve the current process.
+  Did thing by moving a company to modern web development processes
+
+  Like the follow all the latest trends but want to know why A is chosen instead of B
+
+  have a passion for breathing life into designs and putting them onto a page
+
+  My interests outside of coding inclde lifting heavy weights, window shopping, eating lots of food and of course, Sharon Wang.
+
+  My greatest weakness in my opinion is naming variables.
+
+  My personal goal in life is to be part of a project where in the end of the day, I can walk down the street, glance to my side, and be able to whisper "I made that" to myself.
+`;
