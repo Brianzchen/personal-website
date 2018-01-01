a6e1d27e95fff8de86f2e61df633cae907daea2b
Brian Chen <bchen.chen93@gmail.com>
2018-01-01 21:20:37 +1300

    added primitive photo scroll effect

--- src/App/AboutMe/Details/Photo.js
+++ src/App/AboutMe/Details/Photo.js
@@ -1,17 +1,66 @@
 import React from 'react';
 
-const style = {
-  height: '128px',
-  width: '128px',
-  marginLeft: 'auto',
-  background: 'url("public/images/profile.jpg")',
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  borderRadius: '99px',
-};
 
-const Photo = () => (
-  <div style={style} />
-);
+class Photo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scale: 1,
+    };
+
+    window.addEventListener('scroll', this.scroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scroll);
+  }
+
+  scroll = () => {
+    const container = this.container.getBoundingClientRect();
+
+    if (container.y > window.innerHeight / 2) {
+      this.setState({ scale: 1.5 });
+    } else {
+      this.setState({ scale: 1 });
+    }
+  }
+
+  render() {
+    const transform = `scale(${this.state.scale})`;
+
+    const styles = {
+      container: {
+        height: '128px',
+        width: '128px',
+        marginLeft: 'auto',
+        borderRadius: '99px',
+        overflow: 'hidden',
+      },
+      img: {
+        height: '100%',
+        width: '100%',
+        background: 'url("public/images/test.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        MozTransition: 'transform 0.3s',
+        WebkitTransition: 'transform 0.3s',
+        transition: 'transform 0.3s',
+        MozTransform: transform,
+        WebkitTransform: transform,
+        transform,
+      },
+    };
+
+    return (
+      <div
+        ref={o => { this.container = o; }}
+        style={styles.container}
+      >
+        <div style={styles.img} />
+      </div>
+    );
+  }
+}
 
 export default Photo;
