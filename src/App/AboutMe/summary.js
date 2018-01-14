cd6a221edb07df2b98527723d1572eaafdcdd027
Brian Chen <bzchen93@gmail.com>
2018-01-14 22:50:27 +1300

    updated summary

--- src/App/AboutMe/summary.js
+++ src/App/AboutMe/summary.js
@@ -1,28 +1,14 @@
 export default `
-  Self taught and self motivated JavaScript dev, passionate about the front-end world. Always striving for relentless improvement and a stronger believer that if you want to improve you need a personal project.
+  I'm a self-taught and self-motivated JavaScript developer, passionate about the front-end world, always striving for improvement and a strong believer that if you want to improve, you need a personal project.
 
-  Experienced ReactJS developer with strong CSS3 skills. Confident in recreating any design, from image to interactive page.
+  Experienced ReactJS + Redux developer with strong CSS3 skills. Confident in turning any design into an interactive page.
+  Have experience being the lead developer in creating a company in-house ReactJS-based GUI framework, that focuses on performance and cross-browser support.
 
+  I'm a forward thinker who is always looking at ways to improve the current process, from experimenting with running an agile scrum retrospective in an action-oriented way, to transitioning a company's JavaScript development from global scoped ECMAScript 5 to modern moduled ECMAScript 6+.
 
-  I like to develop at home, learn things and take that knowledge back to work and improve my work project and vice versa.
+  I want to be part of a project that is useful to those around me, to see my contributions in use and be proud that I helped to create it.
 
+  In my free time I like to work on my personal projects, go to the gym and watch YouTube.
 
-  Self taught Front-end JavaScript developer focused on ReactJS + Redux.
-
-  Advodate for agile and scrum. Not interested in a scrum master role but very passionate about the agile methodology and implementing it best i can.
-
-  love to code in my free time and learn about new ways of doing old things more efficiently
-
-  always thinking about how i can do things better and improve the current process.
-  Did thing by moving a company to modern web development processes
-
-  Like the follow all the latest trends but want to know why A is chosen instead of B
-
-  have a passion for breathing life into designs and putting them onto a page
-
-  My interests outside of coding inclde lifting heavy weights, window shopping, eating lots of food and of course, Sharon Wang.
-
-  My greatest weakness in my opinion is naming variables.
-
-  My personal goal in life is to be part of a project where in the end of the day, I can walk down the street, glance to my side, and be able to whisper "I made that" to myself.
+  I also love agile and a damn good user experience.
 `;
