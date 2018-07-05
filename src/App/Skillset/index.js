eb9bfa3b00dfdc39ee77177aa1c625260dc47e85
Brian <bzchen93@gmail.com>
2018-07-05 20:14:50 +1000

    update skillset

--- src/App/Skillset/index.js
+++ src/App/Skillset/index.js
@@ -6,18 +6,17 @@ import { skillset } from 'lib/locations';
 
 import Skill from './Skill';
 
-const style = {
-};
-
 const Skillset = () => (
   <Section
-    style={style}
     location={skillset}
   >
     <Skill name="JavaScript" />
     <Skill name="ReactJS" />
     <Skill name="Redux" />
     <Skill name="Client-side routing" />
+    <Skill name="Webpack" />
+    <Skill name="Babel" />
+    <Skill name="SPA/PWA best practices" />
     <Skill name="HTML5" />
     <Skill name="CSS3" />
   </Section>
